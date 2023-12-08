14d56b45883eeb709b432d644e57aa9676436ebd
Antonio <antonio-emilio_6@hotmail.com>
2023-12-08 12:09:45 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Department dp = new Department("books",1);
+		System.out.println(dp);
+
+	}
+
+}
